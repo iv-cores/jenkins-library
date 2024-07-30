4739fde615d0990281a46ff1c74bbce22ad9304b
isaiah.v <isaiah.v@comcast.net>
2024-07-29 23:54:36 -0700

    test

--- src/org/ivcode/jenkins/models/DockerImageInfo.groovy
+++ src/org/ivcode/jenkins/models/DockerImageInfo.groovy
@@ -13,10 +13,10 @@ class DockerImageInfo {
 
         def dockerImageInfo = new DockerImageInfo()
 
-        dockerImageInfo.name = notNull(options['name'] as String, "docker image name is required")
-        dockerImageInfo.tags = notNull(options['tags'] as List<String>, "docker image tags are required")
-        dockerImageInfo.tags = options['file'] as String ?: "./Dockerfile"
-        dockerImageInfo.path = options['path'] as String ?: "."
+        dockerImageInfo.name = notNull(options['name'], "docker image name is required")
+        dockerImageInfo.tags = notNull(options['tags'], "docker image tags are required")
+        dockerImageInfo.file = options['file'] ?: "./Dockerfile"
+        dockerImageInfo.path = options['path'] ?: "."
 
         return dockerImageInfo
     }
