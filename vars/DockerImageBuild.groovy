99aff10da57e950bad346f7d393685fbb6162f9b
isaiah.v <isaiah.v@comcast.net>
2024-07-29 23:24:53 -0700

    yep. testing.

--- vars/DockerImageBuild.groovy
+++ vars/DockerImageBuild.groovy
@@ -6,25 +6,26 @@ import static org.ivcode.jenkins.utils.ScmUtils.isPrimary
 def call(
     Map<String, Object> options = [:]
 ) {
-    def info = DockerImageInfo.fromOptions(options)
-
-    properties([
-        parameters([
-                booleanParam(name: 'publish docker', defaultValue: isPrimary, description: 'publish to the docker repository'),
-                string(name: 'publish tags', defaultValue: "${defaultTags.join(',')}", description: 'comma seperated list of tags to publish')
-        ])
-    ])
 
     node {
         checkout scm
 
+        def info = DockerImageInfo.fromOptions(options)
         def isPrimary = isPrimary(this)
+
+        properties([
+                parameters([
+                        booleanParam(name: 'publish docker', defaultValue: isPrimary, description: 'publish to the docker repository'),
+                        string(name: 'publish tags', defaultValue: "${info.tags.join(',')}", description: 'comma seperated list of tags to publish')
+                ])
+        ])
+
         def isPublish = params['publish docker'] ?: isPrimary
-        def tags = splitTags(params['publish tags'] as String) ?: defaultTags
+        def tags = splitTags(params['publish tags'] as String) ?: info.tags
 
         def image = null;
         stage('Build Docker Image') {
-            image = docker.build("${imageName}:latest", "--file ${file} ${path}")
+            image = docker.build("${info.name}:latest", "--file ${file} ${path}")
         }
 
         stage('Publish Docker Image') {
