d165220a7ab003e8a3a525b4b69134300d5d47af
isaiah.v <isaiah.v@comcast.net>
2024-09-06 12:07:47 -0700

    gradle plugin

---
+++ src/org/ivcode/jenkins/models/GradlePluginInfo.groovy
@@ -0,0 +1,14 @@
+package org.ivcode.jenkins.models
+
+class GradlePluginInfo {
+    /* Docker image to use for building the plugin */
+    public String buildImage = 'registry.ivcode.org/corretto-ubuntu:21-jammy'
+
+    static GradlePluginInfo fromOptions(Map<String, Object> options) {
+        def gradlePluginInfo = new GradlePluginInfo()
+
+        gradlePluginInfo.buildImage = notNull(options['buildImage'] ?: gradlePluginInfo.buildImage, "buildImage is required")
+
+        return gradlePluginInfo
+    }
+}
